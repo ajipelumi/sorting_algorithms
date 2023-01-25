08c1979a08ee8c1104b3d4d2b476bf992f228afd
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-01-25 01:28:07 +0100

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,12 +12,11 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	int temp, flag;
+	int temp;
 	size_t i, j;
 
 	for (i = 0; i < (size - 1); i++) /* iterate size times */
 	{
-		flag = 0; /* flag to track if swap occurs */
 		for (j = 0; j < (size - 1); j++) /* iterate size times again */
 		{
 			if (array[j] > array[j + 1]) /* if current number is greater than next */
@@ -25,7 +24,6 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j]; /* temp holds first integer */
 				array[j] = array[j + 1]; /* swap occurs */
 				array[j + 1] = temp;
-				flag = 1; /* flag is updated to indicate swap */
 				print_array(array, size); /* print every time swap occurs */
 			}
 		}
